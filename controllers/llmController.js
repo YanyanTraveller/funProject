d620fa3790f3e73dfa71471f6ec640624b7e5204
Jian Gao <jiagao@microsoft.com>
2023-02-17 10:53:18 +0800

    Move handler function to controller

--- controllers/llmController.js
+++ controllers/llmController.js
@@ -2,60 +2,68 @@ const fs = require("fs");
 const path = require("path");
 
 const ENDPOINT = "https://httpqas26-frontend-qasazap-prod-dsm02p.qas.binginternal.com/completions";
-let token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiI2OGRmNjZhNC1jYWQ5LTRiZmQtODcyYi1jNmRkZGUwMGQ2YjIiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vNzJmOTg4YmYtODZmMS00MWFmLTkxYWItMmQ3Y2QwMTFkYjQ3L3YyLjAiLCJpYXQiOjE2NzY1MzkwNTAsIm5iZiI6MTY3NjUzOTA1MCwiZXhwIjoxNjc2NTQzMjQyLCJhaW8iOiJBV1FBbS84VEFBQUFhZkJaSlZzMW0yd0dJQ0Jua0FKTFdWUU5JT3dVUVp2ZWpNRFdkdmFmcE5CWjM3ZUx0SzVrTURicnl3dUp0dE1wUTFJYlRsU0d0MnhTU21mN0RTU29ZM21adHpCeHlKazE0azhxNkJLN05Yd3I4ZTFtbHV1YmRFamFIdGdKdCtuQyIsImF6cCI6IjY4ZGY2NmE0LWNhZDktNGJmZC04NzJiLWM2ZGRkZTAwZDZiMiIsImF6cGFjciI6IjAiLCJlbWFpbCI6Inlhbnlhbm11QG1pY3Jvc29mdC5jb20iLCJuYW1lIjoiWWFueWFuIE11Iiwib2lkIjoiMGEyMWZiZTgtOGJmMC00Y2Q2LTg1Y2QtZDI0MGMzMDU2ZGYxIiwicHJlZmVycmVkX3VzZXJuYW1lIjoieWFueWFubXVAbWljcm9zb2Z0LmNvbSIsInJoIjoiMC5BUUVBdjRqNWN2R0dyMEdScXkxODBCSGJSNlJtMzJqWnl2MUxoeXZHM2Q0QTFySWFBR3MuIiwic2NwIjoiYWNjZXNzIiwic3ViIjoiSVJnblp4VW4wS1BPZXhDdlBaZWdzMllHMk41Rmk5dDN3Z1dNTWQ2T0xwNCIsInRpZCI6IjcyZjk4OGJmLTg2ZjEtNDFhZi05MWFiLTJkN2NkMDExZGI0NyIsInV0aSI6IndzY3FSNDVyNGtXbjRBQ21CSXdFQUEiLCJ2ZXIiOiIyLjAiLCJ2ZXJpZmllZF9wcmltYXJ5X2VtYWlsIjpbInlhbnlhbm11QG1pY3Jvc29mdC5jb20iXX0.T4KsPvq-uEs-NymYDxyyeHOvNTZWH9MgDafY8jS0mW2C0EVB9KNWiaqo0tjaO_4cmApjKjX5dejnCIP2XoRm34GTHes0WrYTUIT2Ao7qGwGIVV2anMSSlF85dAapdsOf2PkwIhzvLyYtnpKSPuHzcwYBQnboI3RTXi58nqmXDl2316yIRMTEXGY2pdH-pV79ltFqOdo8fwUamz9YwJels4KyPDX1tpL9LKuSsQRWE-yabJNQ9hhHm3YpAV2-UPppkmQAfxx_XqYCo832L77sW1UQKmR80RMioAEMv-_gHtogTzUz9IVqMf5H5FVbkQI5OotsNpwrnMAk28m_NURdKw"
+let token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiI2OGRmNjZhNC1jYWQ5LTRiZmQtODcyYi1jNmRkZGUwMGQ2YjIiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vNzJmOTg4YmYtODZmMS00MWFmLTkxYWItMmQ3Y2QwMTFkYjQ3L3YyLjAiLCJpYXQiOjE2NzY2MDA4MjIsIm5iZiI6MTY3NjYwMDgyMiwiZXhwIjoxNjc2NjA1OTQ5LCJhaW8iOiJBV1FBbS84VEFBQUE3M1dBTFFkUExDcFhEQS9WaHRKSVdHU0diK0E1VzJRVXBtWHR5RDNHSkdSWkVpc21RbDNtVE5QUXI2QmJzWUN4WkFISzJkeUpHejRCWEhKT2I1bUZtU3M0allGSFJXZ1pYMEFYNjk1OHhEUnNpUU5VZm9PSFZWeitDUU52WGdEbiIsImF6cCI6IjY4ZGY2NmE0LWNhZDktNGJmZC04NzJiLWM2ZGRkZTAwZDZiMiIsImF6cGFjciI6IjAiLCJlbWFpbCI6ImppYWdhb0BtaWNyb3NvZnQuY29tIiwibmFtZSI6IkppYW4gR2FvIiwib2lkIjoiODI4YjkzNmUtODlhNy00MzI4LTg5NGMtMjM0Nzc3ZGQ4OGNkIiwicHJlZmVycmVkX3VzZXJuYW1lIjoiamlhZ2FvQG1pY3Jvc29mdC5jb20iLCJyaCI6IjAuQVFFQXY0ajVjdkdHcjBHUnF5MTgwQkhiUjZSbTMyalp5djFMaHl2RzNkNEExcklhQU5JLiIsInNjcCI6ImFjY2VzcyIsInN1YiI6InBNTV9iWGlma3JyVDBOUmpQVmVhM1ZhVGlDS05pSlMyVzdGS0Z1MzRqQjAiLCJ0aWQiOiI3MmY5ODhiZi04NmYxLTQxYWYtOTFhYi0yZDdjZDAxMWRiNDciLCJ1dGkiOiI5ZHpQdkxNaXZFVy15dlBWWERrSEFBIiwidmVyIjoiMi4wIiwidmVyaWZpZWRfcHJpbWFyeV9lbWFpbCI6WyJqaWFnYW9AbWljcm9zb2Z0LmNvbSJdfQ.LC4sycFG-v-Os_hkemvCx_1HM-Retg1JDJwtyYudAydZQkxTv4pfSVJ3_x9ww2OFqGl2KODLFMdmk36kM962KXVIR_0V76eblhfVavhlsJEfv1U_vA4zkhip6iJbJGCeD-sH0grlWDOKNVN-Dd8OEL6YOKkw-OMY-vsjyIRS-T4P_QDV_WcmaGQ8XPXPe3pFFEua9sXknUbI4JUGiLwhFFgAKyAI-Wkk2ms5Zak1B4ZRVQIeh0uqvhLwxa3ldo7gyiwpdrK1CbSVnS8kmAZKPc10eMIWxazcyEDvHnth02N_CZGLH7FIhX1OV8Ul_bmjNNp0iBKM5x0AlZWIMncnbQ"
 
-exports.sendRequest = async (prompt) => {
-    const response = await fetch(ENDPOINT, {
-        method: "POST",
-        headers: {
-            "Content-Type":"application/json",
-            "Authorization": `Bearer ${token}`,
-            "X-ModelType": "text-davinci-003"
-        },
-        body: JSON.stringify({
-            "prompt": prompt,
-            "max_tokens": 1500,
-            "temperature": 1,
-            "top_p": 1,
-            "n": 1,
-            "stream": false,
-            "logprobs": null,
-            "stop": null
-      })
-    });
-    
-    return await response.json();
+async function sendRequest(prompt) {
+  const response = await fetch(ENDPOINT, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`,
+      "X-ModelType": "text-davinci-003"
+    },
+    body: JSON.stringify({
+      "prompt": prompt,
+      "max_tokens": 1500,
+      "temperature": 1,
+      "top_p": 1,
+      "n": 1,
+      "stream": false,
+      "logprobs": null,
+      "stop": null
+    })
+  });
+
+  return await response.json();
 }
 
 exports.test = async (_, res) => {
-    const responseJSON = await sendRequest("Portland is a city that");
-    res.json(responseJSON);
+  const responseJSON = await sendRequest("Portland is a city that");
+  res.json(responseJSON);
+}
+
+async function sendSearchRequest(query) {
+  const fileList = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../resources/images.json")));
+
+  let prompt = "";
+
+  for (const file of fileList) {
+    prompt += "name: " + file.name + "\n" + "url: " + file.url + "\n";
+    prompt += "\n";
+  }
+
+  prompt += "query: " + query + "\n";
+  prompt += "\n";
+
+  prompt += "result (JSON, name and url fields):\n"
+
+  console.log(prompt);
+
+  const responseJSON = await sendRequest(prompt);
+
+  console.log(responseJSON);
+
+  return JSON.parse(responseJSON.choices[0].text);
 }
 
 exports.getSearchResult = async (req, res) => {
-    const fileList = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../resources/images.json"))); 
+  res.json(await sendSearchRequest(req.params.query));
+}
 
-    let prompt = ""; 
-
-    for (const file of fileList) {
-        prompt += "name: " + file.name + "\n" + "url: " + file.url + "\n";
-        prompt += "\n";
-    }
-
-    prompt += "query: " + req.query.query + "\n";
-    prompt += "\n";
-
-    prompt += "result (JSON, name and url fields):\n"
-
-    console.log(prompt);
-
-    const responseJSON = await sendRequest(prompt);
-
-    console.log(responseJSON);
-
-    const searchResults = JSON.parse(responseJSON.choices[0].text);
-
-    res.json(searchResults);
+exports.getSearchPage = async (req, res) => {
+  const searchQeury = req.query.query;
+  const searchResult = await sendSearchRequest(searchQeury);
+  res.render("search", {data: searchResult, query: searchQeury});
 }
 
 const msal = require("@azure/msal-node");
@@ -71,7 +79,7 @@ const config = {
       "scopes": ["api://68df66a4-cad9-4bfd-872b-c6ddde00d6b2/access"]
     }
   }
-}
+};
 
 const msalApp = new msal.PublicClientApplication({ "auth": config.authOptions });
 
@@ -90,6 +98,6 @@ async function acquireToken() {
 }
 
 exports.getToken = async (_, res) => {
-    token = await acquireToken();
-    res.end(token);
+  token = await acquireToken();
+  res.end(token);
 }
--- routes/routes.js
+++ routes/routes.js
@@ -8,31 +8,8 @@ const router = express.Router();
 router.get('/refreshToken', appController.getToken);
 
 router.get('/api/llm-test', appController.test);
-router.get('/api/searchResult', async (req,res)=>{
-    const fileList = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../resources/images.json"))); 
+router.get('/api/search/:query', appController.getSearchResult);
 
-    let prompt = ""; 
-
-    for (const file of fileList) {
-        prompt += "name: " + file.name + "\n" + "url: " + file.url + "\n";
-        prompt += "\n";
-    }
-
-    prompt += "query: " + req.query.query + "\n";
-    prompt += "\n";
-
-    prompt += "result (in JSON format, name and url fields):\n"
-
-    console.log(prompt);
-
-    const responseJSON = await appController.sendRequest(prompt);
-
-    console.log(responseJSON);
-
-    const data = JSON.parse(responseJSON.choices[0].text);
-
-
-    res.render("result",{data:data, query: req.query.query});
-});
+router.get('/page/search', appController.getSearchPage)
 
 module.exports = router;
