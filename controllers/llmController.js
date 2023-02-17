53bb85adf79a7a8a8ccf30243ca9ffd57e655b06
Yanyanmu <yanyanmu@microsoft.com>
2023-02-17 17:09:02 +0800

    rerender the result page

--- controllers/llmController.js
+++ controllers/llmController.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const path = require("path");
 
 const ENDPOINT = "https://httpqas26-frontend-qasazap-prod-dsm02p.qas.binginternal.com/completions";
-let token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiI2OGRmNjZhNC1jYWQ5LTRiZmQtODcyYi1jNmRkZGUwMGQ2YjIiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vNzJmOTg4YmYtODZmMS00MWFmLTkxYWItMmQ3Y2QwMTFkYjQ3L3YyLjAiLCJpYXQiOjE2NzY2MTg2NzAsIm5iZiI6MTY3NjYxODY3MCwiZXhwIjoxNjc2NjIzNDU2LCJhaW8iOiJBV1FBbS84VEFBQUFuV2NNKzd3RHpNQnRXMjVOVFBpOW1wZ1ZNNTVZekZiQ2htSVR5aU5FRVdWcTJsaGZaVlhDNEJydUxiNHo3SlBRLy90MzdhQTBBWDBuMkEzL1d0YVd6Uk14KzNBMzU2YkRxZ0tPV2g4RkRNc3pIdng3a01WQ3F0dGZ5NlNPbEM5SiIsImF6cCI6IjY4ZGY2NmE0LWNhZDktNGJmZC04NzJiLWM2ZGRkZTAwZDZiMiIsImF6cGFjciI6IjAiLCJlbWFpbCI6Inlhbnlhbm11QG1pY3Jvc29mdC5jb20iLCJuYW1lIjoiWWFueWFuIE11Iiwib2lkIjoiMGEyMWZiZTgtOGJmMC00Y2Q2LTg1Y2QtZDI0MGMzMDU2ZGYxIiwicHJlZmVycmVkX3VzZXJuYW1lIjoieWFueWFubXVAbWljcm9zb2Z0LmNvbSIsInJoIjoiMC5BUUVBdjRqNWN2R0dyMEdScXkxODBCSGJSNlJtMzJqWnl2MUxoeXZHM2Q0QTFySWFBR3MuIiwic2NwIjoiYWNjZXNzIiwic3ViIjoiSVJnblp4VW4wS1BPZXhDdlBaZWdzMllHMk41Rmk5dDN3Z1dNTWQ2T0xwNCIsInRpZCI6IjcyZjk4OGJmLTg2ZjEtNDFhZi05MWFiLTJkN2NkMDExZGI0NyIsInV0aSI6IjlmazZILWZSc0VxOUJTQVE0MHdKQUEiLCJ2ZXIiOiIyLjAiLCJ2ZXJpZmllZF9wcmltYXJ5X2VtYWlsIjpbInlhbnlhbm11QG1pY3Jvc29mdC5jb20iXX0.YIue1MrB7h461wqOGHYORgEAz7oQn6HVUEpD73Oc6yaxvMuA5YEjyDAVs0n-obp9Q83NH8kAdEstX_b-5u1qM3PAv7VijGoscvU27vwcWIq3_tljF_T0Q4Tk3UOKTLa28AGwjO_SXEPRV5P7cO38_0Brcn1lyKne8aEJP0rwaz2a2QLfGf62IEzUlym3_-rW8kuuNfD6BQtVCEdxsXB8p2nbXA9pUkzeuQZHpgIpv2bB00i9NyNmvhknzYOu0NCXbid9LQoLHGuVeUFC-sA436VaigppUVi1zIrk3wgE-0ylTRum0E6cKry9uI4G9yp4g1ERKWm-Ir0ue7O8QcJ2fQ"
+let token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiI2OGRmNjZhNC1jYWQ5LTRiZmQtODcyYi1jNmRkZGUwMGQ2YjIiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vNzJmOTg4YmYtODZmMS00MWFmLTkxYWItMmQ3Y2QwMTFkYjQ3L3YyLjAiLCJpYXQiOjE2NzY2MjM5MTIsIm5iZiI6MTY3NjYyMzkxMiwiZXhwIjoxNjc2NjI4Mzg4LCJhaW8iOiJBV1FBbS84VEFBQUEwYkN6amlFdC9QdWYvNVhNMGh5SEpXQzhjQlZ5Szd0WFpQQXJRSVBDd25ydVJkemlKN3JIN2VQRTRVbFNnZndpMGxOeEgxM2xSRzlFTklkdjBacW02Ui93dE1TWmV4cFJlY0lkc3l6WjFjRkc2V0tCcS9WOFRZVjBSTFFXSytCSSIsImF6cCI6IjY4ZGY2NmE0LWNhZDktNGJmZC04NzJiLWM2ZGRkZTAwZDZiMiIsImF6cGFjciI6IjAiLCJlbWFpbCI6Inlhbnlhbm11QG1pY3Jvc29mdC5jb20iLCJuYW1lIjoiWWFueWFuIE11Iiwib2lkIjoiMGEyMWZiZTgtOGJmMC00Y2Q2LTg1Y2QtZDI0MGMzMDU2ZGYxIiwicHJlZmVycmVkX3VzZXJuYW1lIjoieWFueWFubXVAbWljcm9zb2Z0LmNvbSIsInJoIjoiMC5BUUVBdjRqNWN2R0dyMEdScXkxODBCSGJSNlJtMzJqWnl2MUxoeXZHM2Q0QTFySWFBR3MuIiwic2NwIjoiYWNjZXNzIiwic3ViIjoiSVJnblp4VW4wS1BPZXhDdlBaZWdzMllHMk41Rmk5dDN3Z1dNTWQ2T0xwNCIsInRpZCI6IjcyZjk4OGJmLTg2ZjEtNDFhZi05MWFiLTJkN2NkMDExZGI0NyIsInV0aSI6InJiQkhTT0pGMzA2d2FsZ3dUOHdQQUEiLCJ2ZXIiOiIyLjAiLCJ2ZXJpZmllZF9wcmltYXJ5X2VtYWlsIjpbInlhbnlhbm11QG1pY3Jvc29mdC5jb20iXX0.KLw80kZxJlYBUq2yPLb8KjzzsP4CSaPGtIt5G8rpFFT6kfMkUQsQTUZSMVfjzRMQqI4KPwiBONv9BVVLOpfeShZXGrz0fURH33gXIuPcEXzELYrK5qBWKC8HcfsAi39zfpdGJaOLqyQv-aU-s3DLcnd6BgTQFY-o6FqHyEBE2tu8AstYKXtE6ggaYkVQx06zJOkBJ59g81RNz9c3RCqjuva6Ir6kFwBdiXFYP04YdtMpM9IrcRoPKuELNojckSiVFZcSfrdG0X-4LUaQzY00OxbHPuHYBx2uiK_Qfhgl5dKxctglEwNjMTM8uN2ADuRtwLNl4Ql5nAMfdpdiYMzvPQ"
 
 async function sendRequest(prompt) {
   const response = await fetch(ENDPOINT, {
@@ -64,14 +64,22 @@ exports.getSearchResult = async (req, res) => {
 
 exports.getSearchPage = async (req, res) => {
   const searchQeury = req.query.query;
-  const searchResult = await sendSearchRequest(searchQeury);
-  res.render("search", {data: searchResult, query: searchQeury});
+  try{
+    const searchResult = await sendSearchRequest(searchQeury);
+    res.render("search", {data: searchResult, query: searchQeury,err: null});
+  }catch(err){
+    res.render("search", {err:"No search results.",data:[],query:searchQeury});
+  }
 }
 
 exports.postSearchPage = async (req, res) => {
   const searchQeury = req.body.searchInput; 
-  const searchResult = await sendSearchRequest(searchQeury);
-  res.render("search", {data: searchResult, query: searchQeury});
+  try{
+    const searchResult = await sendSearchRequest(searchQeury);
+    res.render("search", {data: searchResult, query: searchQeury, err: null});
+  }catch(err){
+    res.render("search", {err:"No search results.",data:[],query:searchQeury});
+  } 
 }
 
 const msal = require("@azure/msal-node");
