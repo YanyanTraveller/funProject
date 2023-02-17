530c2d39436b5cbc3aac9976b6cf66456ca8f1fd
Jian Gao <jiagao@microsoft.com>
2023-02-17 14:17:47 +0800

    Use strict mode

--- controllers/llmController.js
+++ controllers/llmController.js
@@ -1,8 +1,10 @@
+"use strict";
+
 const fs = require("fs");
 const path = require("path");
 
 const ENDPOINT = "https://httpqas26-frontend-qasazap-prod-dsm02p.qas.binginternal.com/completions";
-let token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiI2OGRmNjZhNC1jYWQ5LTRiZmQtODcyYi1jNmRkZGUwMGQ2YjIiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vNzJmOTg4YmYtODZmMS00MWFmLTkxYWItMmQ3Y2QwMTFkYjQ3L3YyLjAiLCJpYXQiOjE2NzY2MDA4MjIsIm5iZiI6MTY3NjYwMDgyMiwiZXhwIjoxNjc2NjA1OTQ5LCJhaW8iOiJBV1FBbS84VEFBQUE3M1dBTFFkUExDcFhEQS9WaHRKSVdHU0diK0E1VzJRVXBtWHR5RDNHSkdSWkVpc21RbDNtVE5QUXI2QmJzWUN4WkFISzJkeUpHejRCWEhKT2I1bUZtU3M0allGSFJXZ1pYMEFYNjk1OHhEUnNpUU5VZm9PSFZWeitDUU52WGdEbiIsImF6cCI6IjY4ZGY2NmE0LWNhZDktNGJmZC04NzJiLWM2ZGRkZTAwZDZiMiIsImF6cGFjciI6IjAiLCJlbWFpbCI6ImppYWdhb0BtaWNyb3NvZnQuY29tIiwibmFtZSI6IkppYW4gR2FvIiwib2lkIjoiODI4YjkzNmUtODlhNy00MzI4LTg5NGMtMjM0Nzc3ZGQ4OGNkIiwicHJlZmVycmVkX3VzZXJuYW1lIjoiamlhZ2FvQG1pY3Jvc29mdC5jb20iLCJyaCI6IjAuQVFFQXY0ajVjdkdHcjBHUnF5MTgwQkhiUjZSbTMyalp5djFMaHl2RzNkNEExcklhQU5JLiIsInNjcCI6ImFjY2VzcyIsInN1YiI6InBNTV9iWGlma3JyVDBOUmpQVmVhM1ZhVGlDS05pSlMyVzdGS0Z1MzRqQjAiLCJ0aWQiOiI3MmY5ODhiZi04NmYxLTQxYWYtOTFhYi0yZDdjZDAxMWRiNDciLCJ1dGkiOiI5ZHpQdkxNaXZFVy15dlBWWERrSEFBIiwidmVyIjoiMi4wIiwidmVyaWZpZWRfcHJpbWFyeV9lbWFpbCI6WyJqaWFnYW9AbWljcm9zb2Z0LmNvbSJdfQ.LC4sycFG-v-Os_hkemvCx_1HM-Retg1JDJwtyYudAydZQkxTv4pfSVJ3_x9ww2OFqGl2KODLFMdmk36kM962KXVIR_0V76eblhfVavhlsJEfv1U_vA4zkhip6iJbJGCeD-sH0grlWDOKNVN-Dd8OEL6YOKkw-OMY-vsjyIRS-T4P_QDV_WcmaGQ8XPXPe3pFFEua9sXknUbI4JUGiLwhFFgAKyAI-Wkk2ms5Zak1B4ZRVQIeh0uqvhLwxa3ldo7gyiwpdrK1CbSVnS8kmAZKPc10eMIWxazcyEDvHnth02N_CZGLH7FIhX1OV8Ul_bmjNNp0iBKM5x0AlZWIMncnbQ"
+let token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiI2OGRmNjZhNC1jYWQ5LTRiZmQtODcyYi1jNmRkZGUwMGQ2YjIiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vNzJmOTg4YmYtODZmMS00MWFmLTkxYWItMmQ3Y2QwMTFkYjQ3L3YyLjAiLCJpYXQiOjE2NzY2MTM4MjUsIm5iZiI6MTY3NjYxMzgyNSwiZXhwIjoxNjc2NjE5MzgwLCJhaW8iOiJBV1FBbS84VEFBQUFyN0xaRkVldmJ2cmJlS0RqUWlxZkw4TEdJSE13a1VEUVNXUW1ucWtnUWNjMXM4eVZ6MWJMYnF5YUZzOVMzeVFIVWszb1hNSVpiY3REeGpOdkY2UVBiMzZNbGMxeUZIazgwRWdZdFFBQUdySGhWZFNwUHhLL1gyMTI4RUR2TXhhWSIsImF6cCI6IjY4ZGY2NmE0LWNhZDktNGJmZC04NzJiLWM2ZGRkZTAwZDZiMiIsImF6cGFjciI6IjAiLCJlbWFpbCI6ImppYWdhb0BtaWNyb3NvZnQuY29tIiwibmFtZSI6IkppYW4gR2FvIiwib2lkIjoiODI4YjkzNmUtODlhNy00MzI4LTg5NGMtMjM0Nzc3ZGQ4OGNkIiwicHJlZmVycmVkX3VzZXJuYW1lIjoiamlhZ2FvQG1pY3Jvc29mdC5jb20iLCJyaCI6IjAuQVFFQXY0ajVjdkdHcjBHUnF5MTgwQkhiUjZSbTMyalp5djFMaHl2RzNkNEExcklhQU5JLiIsInNjcCI6ImFjY2VzcyIsInN1YiI6InBNTV9iWGlma3JyVDBOUmpQVmVhM1ZhVGlDS05pSlMyVzdGS0Z1MzRqQjAiLCJ0aWQiOiI3MmY5ODhiZi04NmYxLTQxYWYtOTFhYi0yZDdjZDAxMWRiNDciLCJ1dGkiOiJ0RDJ0WF9iLXprT3NTMUoycnBnSkFBIiwidmVyIjoiMi4wIiwidmVyaWZpZWRfcHJpbWFyeV9lbWFpbCI6WyJqaWFnYW9AbWljcm9zb2Z0LmNvbSJdfQ.Wu4G3lyAy63Ue7pT9IS_CLaX7KNxU_7LADb1N-dnof4CB8VB2z-qv5hkr4MH3GIL34JhWRVCJ_wWe1MWCU15C2m3KXV0P6YEsDW3PLKvA_j4MiZs4bdn-KgTgWgkAcaayr1y5bq8KtN5YzWcytNjBi-ZXjQ2ti000f_XUhtlAB6uAkTBG7dmMp5slDgDsVPrbqysVyB9r0_folCRHEysf6ISlX5QvjD51X6yJGC1N8-4C7Wam6_L59GG0Eqg3LX2_79E0QQaaBehpCA5yBbcMZ7Tq3vzEqVC00oXepLoJchjvY_7ckQeegggoBAEPiR1KgyRxgfO6HQyjZPViVzrjg";
 
 async function sendRequest(prompt) {
   const response = await fetch(ENDPOINT, {
@@ -45,7 +47,7 @@ async function sendSearchRequest(query) {
   prompt += "query: " + query + "\n";
   prompt += "\n";
 
-  prompt += "result (JSON, name and url fields):\n"
+  prompt += "result (JSON, name and url fields):\n";
 
   console.log(prompt);
 
@@ -84,17 +86,8 @@ const config = {
 const msalApp = new msal.PublicClientApplication({ "auth": config.authOptions });
 
 async function acquireToken() {
-  return msalApp
-    .acquireTokenByDeviceCode({ scopes: _SCOPES, deviceCodeCallback: (response) => { console.log(response.message); } })
-    .then((loginResponse) => {
-      if (loginResponse) {
-        return loginResponse.accessToken;
-      }
-    })
-    .catch((error) => {
-      console.error("Acire token error:", error);
-      return "";
-    });
+  const loginResponse = await msalApp.acquireTokenByDeviceCode({ scopes: _SCOPES, deviceCodeCallback: (response) => { console.log(response.message); } });
+  return loginResponse.accessToken;
 }
 
 exports.getToken = async (_, res) => {
